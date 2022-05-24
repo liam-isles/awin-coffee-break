3c834b3cd693fbf4852ad0504bff0f159bb19cc7
liamisles <liam.isles@hotmail.com>
2022-05-24 20:36:55 +0100

    added code to demonstrate where the email would be sent to notify staff members.
    Added dependency to pom for this also.

--- src/main/java/com/awin/coffeebreak/services/EmailNotifier.java
+++ src/main/java/com/awin/coffeebreak/services/EmailNotifier.java
@@ -2,7 +2,11 @@ package com.awin.coffeebreak.services;
 
 import com.awin.coffeebreak.entity.CoffeeBreakPreference;
 import com.awin.coffeebreak.entity.StaffMember;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.mail.Session;
 import java.util.List;
+import java.util.Properties;
 
 public class EmailNotifier {
 
@@ -13,6 +17,27 @@ public class EmailNotifier {
 
         if (staffMember.getEmail().isEmpty()) {
             throw new RuntimeException();
+        }
+
+        String recipient = staffMember.getEmail();
+        String sender = "coffeeMaster@awin.com";
+        String host = "8080";
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", host);
+        // creating session object to get properties
+        Session session = Session.getDefaultInstance(properties);
+        try
+        {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(sender));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+            message.setSubject("Coffee Break Preference");
+            message.setText("Here is your preference! " + preferences.toString());
+            //Transport.send(message);
+        }
+        catch (MessagingException mex)
+        {
+            mex.printStackTrace();
         }
 
         return true;
